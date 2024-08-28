443c56643473820a1ee883c07f13f0bd67590214
yubin0210 <152180495+yubin0210@users.noreply.github.com>
2024-08-28 23:40:34 +0900

    240828 commit

---
+++ 240828/src/main/java/com/itbank/repository/Ex04DAO.java
@@ -0,0 +1,19 @@
+package com.itbank.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import com.itbank.model.Upload2DTO;
+
+public interface Ex04DAO {
+
+	@Insert("insert into upload2 (memo, originalFileName, storedFileName) "
+			+ "	values (#{memo}, #{originalFileName}, #{storedFileName})")
+	int insertMultiple(Upload2DTO dto);
+
+	@Select("select * from upload2 order by idx")
+	List<Upload2DTO> selectList();
+
+}
